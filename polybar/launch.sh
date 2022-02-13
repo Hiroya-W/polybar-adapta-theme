e79307be47f84c39213fb51f9bc4417a480dc78b
Hiroya-W <hyuyukun@gmail.com>
2022-02-14 00:20:52 +0900

    :books: Add link to how to show on all displays

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
 # Launch bar
+# https://github.com/polybar/polybar/issues/763#issuecomment-392960721
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload main &
 done
